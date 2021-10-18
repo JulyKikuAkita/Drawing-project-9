5dde443358154efb941a887319dc860e839977b7
JulyKikuAkita <b92701105@gmail.com>
2021-10-17 21:21:12 -0700

    Initial Commit

---
+++ Drawing-project-9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing-project-9
+//
+//  Created by Ifang Lee on 10/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing-project-9/Drawing_project_9App.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_project_9App.swift
+//  Drawing-project-9
+//
+//  Created by Ifang Lee on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_project_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

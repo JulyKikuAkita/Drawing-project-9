b65b61009132f9dfa7b4330dfed7b49250a0ecb9
JulyKikuAkita <b92701105@gmail.com>
2021-10-21 18:31:04 -0700

    blend and blur

--- Drawing-project-9/ContentView.swift
+++ Drawing-project-9/ContentView.swift
@@ -7,94 +7,50 @@
 
 import SwiftUI
 
-/**
- the even-odd rule, which decides whether part of a path should be colored depending on the overlaps it contains. It works like this:
-
- If a path has no overlaps it will be filled.
- If another path overlaps it, the overlapping part won’t be filled.
- If a third path overlaps the previous two, then it will be filled.
- …and so on.
-
- Important: The drawingGroup() modifier is helpful to know about and to keep in your arsenal as a way to solve performance problems when you hit them, but you should not use it that often. Adding the off-screen render pass might slow down SwiftUI for simple drawing, so you should wait until you have an actual performance problem before trying to bring in drawingGroup().
- */
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var amount: CGFloat = 0.0
 
-    @State private var colorCycle = 0.0
 
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
-
-            Slider(value: $colorCycle)
-        }
-    }
-}
-
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
-
-    var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
+            ZStack {
+                // demo blend effect
                 Circle()
-                    .inset(by: CGFloat(value))
-//                    .strokeBorder(self.color(for: value, brightness: 1), lineWidth: 2)
-                    .stroke(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.3)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+                    .fill(Color(red: 1, green: 0, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(Color(red: 0, green: 1, blue: 0))
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(Color(red: 0, green: 0, blue: 1))
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
             }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+
+            // demo blur effect
+            Image("myFutureNoodles")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(Double(amount))
+                .blur(radius: (1 - amount) * 20)
         }
-        .drawingGroup() //only when need performace boost //Metal, which is Apple’s framework for working directly with the GPU for extremely fast graphics.
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
+
     }
 
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
-    }
-}
-
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
-
-    // How wide to make each petal
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
-        var path = Path()
-
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, through: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
-        }
-
-        // now send the main path back
-        return path
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {

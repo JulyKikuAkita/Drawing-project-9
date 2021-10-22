69f813eb4240960ea5a211218a77c074031e4100
JulyKikuAkita <b92701105@gmail.com>
2021-10-21 19:07:00 -0700

    animatableData and paris

--- Drawing-project-9/ContentView.swift
+++ Drawing-project-9/ContentView.swift
@@ -8,48 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var amount: CGFloat = 0.0
+    @State private var insetAmount: CGFloat = 50
 
+    @State private var rows = 4
+    @State private var columns = 4
+    @State private var color = Color.green
 
     var body: some View {
         VStack {
-            ZStack {
-                // demo blend effect
-                Circle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: -50, y: -80)
-                    .blendMode(.screen)
+            Checkerboard(rows: rows, columns: columns)
+                .foregroundColor(color)
+                .onTapGesture {
+                    withAnimation(.linear(duration: 3)) {
+                        self.rows = 8
+                        self.columns = 8
+                        self.color = .pink
+                    }
+                }
 
-                Circle()
-                    .fill(Color(red: 0, green: 1, blue: 0))
-                    .frame(width: 200 * amount)
-                    .offset(x: 50, y: -80)
-                    .blendMode(.screen)
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 300, height: 150)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
 
-                Circle()
-                    .fill(Color(red: 0, green: 0, blue: 1))
-                    .frame(width: 200 * amount)
-                    .blendMode(.screen)
-            }
-            .frame(width: 300, height: 300)
-
-            Slider(value: $amount)
-                .padding()
-
-            // demo blur effect
-            Image("myFutureNoodles")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .saturation(Double(amount))
-                .blur(radius: (1 - amount) * 20)
+                    }
+                }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black)
-        .edgesIgnoringSafeArea(.all)
 
     }
+}
+
+// animatableData
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    // required data for animation
+    var animatableData: CGFloat {
+        get {insetAmount}
+        set {
+            self.insetAmount = newValue
+        }
+    }
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+    }
+}
+
+//AnimatablePair
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    // animatable data of 2
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    // animatable data of 4:
+//    AnimatablePair<CGFloat, AnimatablePair<CGFloat, AnimatablePair<CGFloat, CGFloat>>>
+// newValue.second.second.first
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+
 
 }
 
